ab456efc5cdecb54d68e85ec07f1055ec2aeb30f
saur0nnn <102294103+saur0nnn@users.noreply.github.com>
2022-04-09 18:23:40 +0400

    Add files via upload

---
+++ logger.py
@@ -0,0 +1,5 @@
+import coloredlogs, logging
+coloredlogs.install()
+logging.info("Test 1235213675467124571264124721")
+logging.warning("Test 1235213675467124571264124721")
+logging.error("Test 1235213675467124571264124721")
